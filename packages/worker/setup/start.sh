2a461849f2c028c3cddd2f18b4dbd4bb5529baea
Boris Okunskiy <boris@okunskiy.name>
2020-09-07 16:36:56 +0300

    fix: add --http-parser=legacy to worker entrypoint

--- packages/worker/setup/start.sh
+++ packages/worker/setup/start.sh
@@ -2,4 +2,4 @@
 ./vnc.sh
 
 export DISPLAY=:1
-exec node --max-http-header-size=65535 ./node_modules/.bin/worker-entrypoint
+exec node --max-http-header-size=65535 --http-parser=legacy ./node_modules/.bin/worker-entrypoint
