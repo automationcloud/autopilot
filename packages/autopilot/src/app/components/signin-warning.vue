35e91d5677351d44c72c243667295b1d86d5c72d
Kyung-eun Kim <kyungeunni@gmail.com>
2020-09-08 14:18:39 +0200

    feat: Icons, help pane and first-run ui update (#10)
    - feat: add help pane (not shown yet)
    - chore: rearrange ac css
    - feat: add new icons for all platform
    - feat: update firstrun UI as per new spec(v1)

--- packages/autopilot/src/app/components/signin-warning.vue
+++ packages/autopilot/src/app/components/signin-warning.vue
@@ -2,20 +2,17 @@
     <div class="signin-warning automation-cloud">
 
         <template v-if="!loggingIn">
-            <div class="box">
+            <div class="box box--primary" style="display: flex; align-items: center;">
                 <i class="fas fa-exclamation-circle"></i>
                 <span>You need to be signed-in {{ message }}.</span>
             </div>
 
-            <div style="margin-top: var(--gap);">
-                <button
-                    class="button button--yellow"
-                    type="click"
-                    @click="$emit('signin');">
-                    <i class="fas fa-angle-right button__icon"></i>
-                    <span>Sign-in</span>
-                </button>
-            </div>
+            <button
+                class="button button--primary"
+                type="click"
+                @click="$emit('signin');">
+                <span>Sign-in</span>
+            </button>
         </template>
     </div>
 </template>
@@ -38,19 +35,5 @@ export default {
     margin: var(--gap);
     color: var(--color-cool--800);
     font-size: 1.2em;
-}
-
-.box {
-    margin: var(--gap--large) 0;
-    font-size: inherit;
-    background-color: var(--color-yellow--300);
-    vertical-align: middle;
-    display: flex;
-    align-items: center;
-}
-
-.box i {
-    font-size: 1.4em;
-    margin-right: var(--gap);
 }
 </style>
---
+++ packages/autopilot/src/app/viewports/ac-help/viewport.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="ac-help automation-cloud">
+        <div class="ac-help-item">
+
+            <article class="content">
+                <div class="content-header">
+                    <h2>Get Help</h2>
+                </div>
+                <p class="content-text">
+                    Graduated from Robot School and still need help?
+                    Start in the Automation Cloud forum.
+                    Expert scripters and the Automation Cloud team monitor all issues and conversations there.
+                </p>
+                <button
+                    class="button button--primary"
+                    type="click"
+                    @click="onLinkClick('comunity')">
+                    <span>Community</span>
+                </button>
+
+                <div class="box box--primary">
+                    <h2 style="margin-top: 0px; font-size: 20px;">Find out more about the Automation Cloud</h2>
+                    <span
+                        style="display: inline-flex; align-items: center; cursor: pointer;"
+                        @click="onLinkClick('comunity')">
+                        <i class="fas fa-angle-right"></i>
+                        automationcloud.net
+                    </span>
+                </div>
+            </article>
+
+            <article class="content">
+                <div class="content-header">
+                    <h2>Tutorials &amp; API Reference </h2>
+                    <img src="resources/product-logos/robot-school.svg" class="content-image">
+                </div>
+                <p class="content-text">
+                    Learn the fundamentals of scripting in Autopilot by following our guided tutorials.
+                    Access reference documentation about the Autopilot tool and the Automation Cloud API.
+                </p>
+                <button
+                    class="button button--primary"
+                    type="click"
+                    @click="onLinkClick('robotSchool')">
+                    <span>Robot School</span>
+                </button>
+            </article>
+
+            <hr class="content">
+
+            <article class="content">
+                <div class="content-header">
+                    <h2>Monitor your Automations</h2>
+                    <img src="resources/product-logos/dashboard.svg" class="content-image">
+                </div>
+
+                <p class="content-text">
+                    Dashboard is the nerve centre for managing, observing and monitoring the authomations you run in the Automation Cloud.
+                    Watch jobs while they execute and drill down into the low-level detail.
+                </p>
+
+                <button
+                    class="button button--primary"
+                    type="click"
+                    @click="onLinkClick('dashboard')">
+                    {{ authorised ? 'Dashboard' : 'Sign-in to your Dashboard' }}
+                </button>
+                <button
+                    v-show="!authorised"
+                    class="button button--tertiary"
+                    type="click"
+                    @click="onLinkClick('signup')">
+                    Sign-up and get started
+                </button>
+            </article>
+        </div>
+
+        <div class="ac-help-item tray-bg--transparent-light">
+            <promo-robot-school/>
+        </div>
+    </div>
+</template>
+
+<script>
+import PromoRobotSchool from '~/components/automationcloud/promo-robot-school.vue';
+import { shell } from 'electron';
+import {
+    ApiLoginController
+} from '~/controllers';
+
+export default {
+    components: {
+        PromoRobotSchool,
+    },
+
+    data() {
+        return {
+            links: {
+                comunity: '#',
+                automationCloud: '#',
+                robotSchool: 'https://robotschool.dev',
+                dashboard: '#',
+                singin: '#',
+                signup: '#'
+            }
+        };
+    },
+
+    computed: {
+        apiLogin() { return this.get(ApiLoginController); },
+        authorised() { return this.apiLogin.authorised; },
+    },
+
+    methods: {
+        onLinkClick(type) {
+            const link = this.links[type];
+            shell.openExternal(link);
+        }
+    }
+
+};
+</script>
+
+<style scoped>
+.ac-help {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-between;
+    font-size: 14px;
+    font-weight: 400;
+}
+
+.ac-help-item {
+    padding: var(--gap) var(--gap--large);
+}
+
+.content {
+    margin-bottom: var(--gap--large);
+}
+
+.content > * {
+    margin-bottom: var(--gap--large);
+}
+
+.content-header {
+    display: flex;
+    justify-content: space-between;
+}
+
+.content-image {
+    width: 110px;
+    margin-left: var(--gap);
+}
+
+.content-text {
+    max-width: 650px;
+}
+
+</style>
