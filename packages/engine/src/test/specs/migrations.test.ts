c09e62761752d1c811852100beb1d01121056c97
Boris Okunskiy <boris@okunskiy.name>
2020-09-23 17:57:32 +0300

    chore: use consistent company casing

--- packages/engine/src/test/specs/migrations.test.ts
+++ packages/engine/src/test/specs/migrations.test.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 Ubio Limited
+// Copyright 2020 UBIO Limited
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
