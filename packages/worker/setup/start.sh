df8dbb49169ad5edd8167135af26fe93ea8946eb
kyungeunni <kyungeunni@gmail.com>
2021-05-17 21:46:39 +0200

    fix: add helper js starting entrypoint

--- packages/worker/setup/start.sh
+++ packages/worker/setup/start.sh
@@ -2,4 +2,4 @@
 ./vnc.sh
 
 export DISPLAY=:1
-exec node --max-http-header-size=65535 --insecure-http-parser ./node_modules/.bin/worker-entrypoint
+exec node --max-http-header-size=65535 --insecure-http-parser ./start
